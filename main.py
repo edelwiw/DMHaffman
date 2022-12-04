0846cca23551035dd0c5a0045ec6cf7777c9e662
edelwiw <edelwiw@gmail.com>
2022-12-04 18:19:51 +0300

    Upd

--- main.py
+++ main.py
@@ -5,75 +5,65 @@ import sys
 class Node(object):
 
     def __init__(self, char=None, value=0, right=None, left=None,):
-        self.value = value
-        self.right = right
-        self.left = left
-        self.char = char
+        self.value = value  # вес ноды ака количество таких символов в тексте
+        self.right = right  # правый ребенок
+        self.left = left  # левый ребенок
+        self.char = char  # символ, для которого работаем
 
-    def __lt__(self, other):
+    def __lt__(self, other):  # less than - для сортировкм нод
         return self.value < other.value
 
-    def __repr__(self):
+    def __repr__(self): # represent - по большой части для дебага, в проде можно будет убрать. возвращает описание ноды
         return "\"" + str(self.char) + "\"-" + str(self.value) + " (" + str(self.right) + " " + str(self.left) + ")"
 
-    #def __str__(self):
-    #    return "\"" + str(self.char) + "\"-" + str(self.value)
 
+def create_dict(text):
+    dict = {}
+    nodes = []
 
-class Dict(object):
-
-    def __init__(self):
-        self.nodes = []
-        self.dict = {}
-
-    def create_dict(self, text):
-        c = Counter(text)
-        for e in c:
-            self.nodes.append(Node(e, c[e]))
-
-        while len(self.nodes) > 1:
-            self.nodes.sort()
-            m1 = self.nodes.pop(0)
-            m2 = self.nodes.pop(0)
-
-            self.nodes.append(Node(m1.char + m2.char, m1.value + m2.value, m1, m2))
-
-        self.__dfs__(self.nodes[0], "")
-
-    def set_dict(self, dict: dict):
-        self.dict = dict
-
-    def __dfs__(self, node, way):
-        if node.right and node.left:
-            self.__dfs__(node.right, way + "1")
-            self.__dfs__(node.left, way + "0")
+    def dfs(node, way):
+        if node.right:  # если есть ребенок справа
+            dfs(node.right, way + "1") # запускаем для правого, записываем путь
+        if node.left:
+            dfs(node.left, way + "0")  # для левого
         else:
-            self.dict[node.char] = way
+            dict[node.char] = way  # дошли до конца - записываем в словарь код символа (путь)
             return
 
-    def get_dict(self):
-        return self.dict
+    c = Counter(text)  # считаем количество символов
+    for e in c:
+        nodes.append(Node(e, c[e])) # создаем ноду для каждого символа
 
-    def encode(self, text):
-        encoded = ""
-        for el in text:
-            encoded += self.dict[el]
-        return encoded
+    while len(nodes) > 1:
+        nodes.sort()  # сортируем и берем 2 самых маленьких ноды (те, символов которых меньше всего в тексте)
+        m1 = nodes.pop(0)
+        m2 = nodes.pop(0)
+        nodes.append(Node(m1.char + m2.char, m1.value + m2.value, m1, m2))  # содаем ноду-родителя, символы конкатенируем, веса складываем
 
-    def decode(self, text):
-        letters = {value: key for key, value in self.dict.items()}
-        decoded = ""
-        buff = ""
+    dfs(nodes[0], "")  # запускаем дфс
+    return dict
 
-        for i in text:
-            buff += i
-            if buff in letters:
-                decoded += letters[buff]
-                buff = ""
-        return decoded
 
-    def __repr__(self):
-        return str(self.dict)
+
+
+def encode(text, dict):  # просто бежим по символам и записываем их код
+    encoded = ""
+    for el in text:
+        encoded += dict[el]
+    return encoded
+
+
+def decode(text, dict):
+    letters = {value: key for key, value in dict.items()}  # транспонируем словарь
+    decoded = ""
+    buff = ""
+
+    for i in text:
+        buff += i  # добавляем в буффер новый бит
+        if buff in letters:  # если встретился символ с таким кодом, записываем. Коды соотв. правилу фано - однозначное трактование
+            decoded += letters[buff]
+            buff = ""
+    return decoded
 
 
 if __name__ == '__main__':
@@ -83,20 +73,46 @@ if __name__ == '__main__':
     in_path = sys.argv[2]
     out_path = sys.argv[3]
 
-
-    inf = open(in_path, 'rb')
-    ouf = open(out_path, 'wb')
-
-    d = Dict()
-
     if method == '--encode':
+        inf = open(in_path, 'r')
+        ouf = open(out_path, 'wb')
+
         text = inf.read()
-        d.create_dict(text)
-        encoded = d.encode(text)
+        print("Input text = " + text)
 
+        dict = create_dict(text)  # создаем словарь с кодами
+        ouf.write((str(len(dict)) + '\n').encode())  # пишем длину в фаил
 
-        for i in
-        print(encoded)
+        for el in dict:
+            ouf.write((el + ' ' + str(dict[el]) + '\n').encode())
+
+        encoded = encode(text, dict)
+        print("Encoded text = " + encoded)
+        ouf.write(encoded.encode())
+
+        inf.close()
+        ouf.close()
+
+    if method == '--decode':
+        inf = open(in_path, 'rb')
+        ouf = open(out_path, 'w')
+
+        dict = {}
+
+        l = int(inf.readline())
+
+        for i in range(l):
+            line = inf.readline()
+            dict[chr(line[0])] = line[2:-1].decode()
+
+        text = inf.readline().decode()
+        decoded = decode(text, dict)
+        print("Decoded text = " + decoded)
+        ouf.write(decoded)
+
+        inf.close()
+        ouf.close()
+
 
 
 
